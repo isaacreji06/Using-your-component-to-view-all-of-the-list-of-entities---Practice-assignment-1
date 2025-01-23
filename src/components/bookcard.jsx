32999f8a0be41602611416f1a3e777be39800abe
Isaac Reji <isaac.reji@kalvium.community>
2025-01-23 10:38:33 +0530

    Assignment completed

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,20 @@
 // write the book component code here
+import React from 'react'
+
+function Bookcard({
+    image,
+    name,
+    genre,
+    author
+}) {
+  return (
+    <div style={{background:'Red',display:'grid', justifyContent:'center'}}>
+        <img style={{height:200,justifyContent:'center'}} src={image} alt="" />
+        <p style={{color:'black'}}><strong>Name:</strong>{name}</p>
+        <p style={{color:'black'}}><strong>Genre:</strong>{genre}</p>
+        <p style={{color:'black'}}><strong>Author:</strong>{author}</p>
+    </div>
+  )
+}
+
+export default Bookcard
